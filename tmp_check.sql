0f7b9a9e6e5e18bddb01e438bc906207d362f369
artem7vorobev-afk <artem7.vorobev@gmail.com>
2026-09-12 12:35:16 +0300

    chore: remove temp debug file

--- tmp_check.sql
+++
@@ -1,3 +0,0 @@
-SELECT id, "telegramId", "firstName", "referralCode", "referredBy", "createdAt"
-FROM "User" ORDER BY "createdAt" DESC LIMIT 10;
-SELECT * FROM "Referral" ORDER BY "createdAt" DESC LIMIT 10;
